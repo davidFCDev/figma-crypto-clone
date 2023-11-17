815be7dfe520d055985e65feeb37aa5ad2dfa997
David Fernández <davidfernandezcomesana@gmail.com>
2023-11-17 20:27:13 +0100

    error hero corrected

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -4,14 +4,14 @@ import Resume from "./Resume";
 
 const Hero = () => {
   return (
-    <main className="flex flex-col min-h-screen w-full ">
-      <div className="w-full justify-between items-start gap-12 small:gap-0 flex flex-col small:flex-row small:pl-28 pt-28 small:pt-40  screen-path">
-        <div className="flex flex-col gap-6 items-start pt-10 small:pr-4 pr-6">
-          <h1 className="text-5xl small:text-6xl font-semibold tracking-tight small:tracking-normal ">
+    <main className="flex flex-col min-h-screen w-full">
+      <div className="w-full justify-between items-start gap-12 small:gap-0 flex flex-col small:flex-row small:pl-28 pt-28 small:pt-40 px-6 small:px-0 screen-path">
+        <div className="flex flex-col gap-6 items-start pt-10 small:pr-4">
+          <h1 className="text-4xl small:text-6xl font-semibold tracking-tight small:tracking-normal ">
             Create clever incentives and utility for all your decentralized
             products
           </h1>
-          <p className={`${styles.parraph} pr-2 small:pr-0`}>
+          <p className={`${styles.parraph} pr-2 small:pr-0 text-sm small:text-lg`}>
             By harnessing the power of blockchain technology, LockPay empowers
             businesses, projects, and brands to create customizable lockers
             tailored to their unique requirements.
